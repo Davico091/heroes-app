19ec7c76a425a28e02462c21780e89de20a46c8e
David <perzc.david@gmail.com>
2018-02-18 21:11:16 -0500

    add sources

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+    {id : 1 ,name : 'ciclope'},
+    {id : 2 ,name : 'worverine'},
+    {id : 3 ,name : 'tormenta'},
+    {id : 4 ,name : 'bestia'},
+    {id : 5 ,name : 'arcaangel'},
+    {id : 6 ,name : 'magneto'},
+    { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
